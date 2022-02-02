98eac77d12d990fd6aa677689bf5ce9fb063d656
jeongminko <terrantrace@gmail.com>
2022-02-03 02:05:36 +0900

    ios first clone project

---
+++ Today-I-Learned-main/IOS/ExampleProject/ExampleProject/ExampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleProjectApp.swift
+//  ExampleProject
+//
+//  Created by JeongMin Ko on 2022/02/03.
+//
+
+import SwiftUI
+
+@main
+struct ExampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
