f9de2d56905af94a7b2dd812572365b0ece447f9
eigen98 <terrantrace@gmail.com>
2022-10-12 11:18:56 +0900

    멋쟁이 사자처럼 HTML 데모

--- Today-I-Learned-main/IOS/HelloWorld/HelloWorld/main.m
+++ Today-I-Learned-main/IOS/HelloWorld/HelloWorld/main.m
@@ -8,9 +8,16 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
+    
+    
     @autoreleasepool {
+        
+        int sum = 0;
+        sum = 50 + 25;
+        
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hello, World! \n My name is Eigen");
+        NSLog(@"sum %i", sum);
     }
     return 0;
 }
